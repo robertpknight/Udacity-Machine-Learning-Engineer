860c833d68298a779ea60eccf42bfb5ca9c9b126
Rob Knight <robertpknight@hotmail.com>
2019-12-08 23:04:24 +0000

    release updates with description for Pypi

--- workspace/python_package_development/setup.py
+++ workspace/python_package_development/setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 
 setup(name='gaus_bin_dists',
-      version='0.1',
+      version='0.12',
       description='Gaussian and Binomial distributions',
+      long_description= 'Gaussian and Binomial distributions for Udacity ML Engineer Course',
       packages=['gaus_bin_dists'],
-      author = 'Robert Knight',
-      author_email = 'robertpknight@hotmail.com',
+      author='Robert Knight',
+      author_email='robertpknight@hotmail.com',
       zip_safe=False)
