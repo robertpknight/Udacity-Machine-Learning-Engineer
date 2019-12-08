aefd1c651c7dc1f8a78c23a93a0bdf9baa088db9
Rob Knight <robertpknight@hotmail.com>
2019-12-08 22:30:09 +0000

    Update setup details

--- workspace/python_package_development/setup.py
+++ workspace/python_package_development/setup.py
@@ -4,4 +4,6 @@ setup(name='gaus_bin_dists',
       version='0.1',
       description='Gaussian and Binomial distributions',
       packages=['gaus_bin_dists'],
+      author = 'Robert Knight',
+      author_email = 'robertpknight@hotmail.com',
       zip_safe=False)
