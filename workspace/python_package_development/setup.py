2dc3b415d7c2ff4295cb804242409ba4d793e847
Rob Knight <robertpknight@hotmail.com>
2019-12-08 15:34:12 +0000

    distributions package and virtual env for course

---
+++ workspace/python_package_development/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distibutions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
